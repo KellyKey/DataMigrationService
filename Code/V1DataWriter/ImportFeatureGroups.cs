54324b029a4232ea5292c41fe3c6b8669737f7ca
kellykey <kellykey@KELLYKEY9EBA>
2019-09-02 16:12:32 -0500

    Updated ImportFeatureGroups for EmbeddedImages

--- Code/V1DataWriter/ImportFeatureGroups.cs
+++ Code/V1DataWriter/ImportFeatureGroups.cs
@@ -48,7 +48,10 @@ namespace V1DataWriter
                     asset.SetAttributeValue(fullNameAttribute, AddV1IDToTitle(sdr["Name"].ToString(), sdr["AssetNumber"].ToString()));
 
                     IAttributeDefinition descAttribute = assetType.GetAttributeDefinition("Description");
-                    asset.SetAttributeValue(descAttribute, sdr["Description"].ToString());
+                    //asset.SetAttributeValue(descAttribute, sdr["Description"].ToString());
+                    string targetURL = _config.V1TargetConnection.Url;
+                    string newDescription = SetEmbeddedImageContent(sdr["Description"].ToString(), targetURL);
+                    asset.SetAttributeValue(descAttribute, newDescription);
 
                     IAttributeDefinition customerAttribute = assetType.GetAttributeDefinition("Customer");
                     asset.SetAttributeValue(customerAttribute, GetNewAssetOIDFromDB(sdr["Customer"].ToString()));
@@ -101,38 +104,6 @@ namespace V1DataWriter
                     UpdateNewAssetOIDAndNumberInDB("FeatureGroups", sdr["AssetOID"].ToString(), asset.Oid.Momentless.ToString(), newAssetNumber);
                     UpdateImportStatus("FeatureGroups", sdr["AssetOID"].ToString(), ImportStatuses.IMPORTED, "Feature group imported.");
                     importCount++;
-
-                    //Get Any EmbeddedImages for the Description Field
-                    int[] imgIDLocation = new int[10];
-                    int imageCounter = 0;
-
-                    SqlDataReader sdrEmbeddedImages = GetDataFromDB("EmbeddedImages", sdr["AssetOID"].ToString());
-                    while (sdrEmbeddedImages.Read())
-                    {
-                        //UploadEmbeddedImageContent(ref asset, asset.Oid,(byte[])sdrEmbeddedImages["Content"], _imageConnector);
-
-                        ////Create an embedded image.
-                        string filePath = @"C:\Windows\Temp\TempImage.png";
-                        System.IO.File.WriteAllBytes(filePath, (byte[])sdrEmbeddedImages["Content"]);
-                        //string file = @"C:\Temp\versionone.jpg";
-                        Oid embeddedImageOid = _dataAPI.SaveEmbeddedImage(filePath, asset);
-
-                        string currentDescription = sdr["Description"].ToString();
-                        imgIDLocation[imageCounter] = currentDescription.IndexOf(".img/", 0, currentDescription.Length);
-                        string firstDescriptionPart = currentDescription.Substring(0, imgIDLocation[imageCounter] + 5);
-                        string secondDescriptionPart = currentDescription.Substring(imgIDLocation[imageCounter] + 5);
-                        imgIDLocation[imageCounter] = secondDescriptionPart.IndexOf(">", 0, secondDescriptionPart.Length);
-                        string newSecondDescriptionPart = secondDescriptionPart.Substring(imgIDLocation[imageCounter]);
-
-                        string embeddedImageTag = string.Format(" {0} alt=\"\" data-oid=\"{1}\" {2}", firstDescriptionPart + embeddedImageOid.Key + "\"", embeddedImageOid.Momentless, newSecondDescriptionPart);
-                        //var embeddedImageTag = string.Format("<p></p></br><img src=\"{0}\" alt=\"\" data-oid=\"{1}\" />", "embedded.img/" + embeddedImageOid.Key, embeddedImageOid.Momentless);
-                        asset.SetAttributeValue(descAttribute, embeddedImageTag);
-                        _dataAPI.Save(asset);
-
-                        UpdateNewAssetOIDAndStatus("EmbeddedImages", sdrEmbeddedImages["AssetOID"].ToString(), embeddedImageOid.Momentless.ToString(), ImportStatuses.IMPORTED, "EmbeddedImage imported.");
-
-                        imageCounter++;
-                    }
 
                 }
                 catch (Exception ex)
